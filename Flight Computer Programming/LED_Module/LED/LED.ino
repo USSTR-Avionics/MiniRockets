1c2654656d1f67323212f5860b26455afef4c7d8
djh293 <48139207+djh293@users.noreply.github.com>
2022-01-02 15:14:37 -0600

    Added LED Module with BLUE, GREEN, RED Functionality

---
+++ Flight Computer Programming/LED_Module/LED/LED.ino
@@ -0,0 +1,78 @@
+/*
+ * LED Module
+ * 
+ * Inputs: BLUE, GREEN, CATHODE, RED
+ * 
+ * Outputs:
+ * - red light
+ * - green light
+ * - blue light
+ * 
+ * Set Up Instructions: 
+ * 1. LED input pin order is BLUE, GREEN, CATHODE, RED with CATHODE having a longer wire
+ * 2. Connect BLUE to 220 ohm resistor, GREEM to a 220 ohm resistor, and RED to a 220 ohm resistor
+ * 3. Connect BLUE to pin 3, GREEN to pin 5, RED to pin 6, and CATHODE to GND
+ * 
+ * Functions:
+ * ledOn(color): changes color to "BLUE", "GREEN",OR "RED" based on color parameter
+ * ledOFF: turns the LED off
+ */
+
+
+
+void setup() {
+  // Define Pins
+  #define BLUE 3
+  #define GREEN 5
+  #define RED 6
+  initSensor();
+}
+
+
+
+void loop() {
+  delay(1000);
+  ledON("RED");
+  delay(1000);
+  ledOFF();
+  delay(1000);
+  ledON("GREEN");
+  delay(1000);
+  ledOFF();
+  delay(1000);
+  ledON("BLUE");
+  delay(1000);
+  ledOFF();
+  delay(1000);
+}
+
+void initSensor() {
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+void ledON(String color) {
+  if (color == "RED") {
+    analogWrite(RED,255);
+    analogWrite(GREEN,0);
+    analogWrite(BLUE,0);
+  }
+  if (color == "GREEN") {
+    analogWrite(RED,0);
+    analogWrite(GREEN,255);
+    analogWrite(BLUE,0);
+  }
+  if (color == "BLUE") {
+    analogWrite(RED,0);
+    analogWrite(GREEN,0);
+    analogWrite(BLUE,255);
+  }
+  
+}
+
+void ledOFF() {
+  analogWrite(RED,0);
+  analogWrite(GREEN,0);
+  analogWrite(BLUE,0);
+}
