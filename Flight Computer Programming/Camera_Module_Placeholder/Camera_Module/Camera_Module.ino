2512ce8728f403401faa74faaeef4888bfc70346
djh293 <48139207+djh293@users.noreply.github.com>
2022-01-04 14:42:19 -0600

    Added camera module placeholder comments

---
+++ Flight Computer Programming/Camera_Module_Placeholder/Camera_Module/Camera_Module.ino
@@ -0,0 +1,26 @@
+/*
+ * Camera Module
+ * 
+ * https://www.runcam.com/download/split4k/RC_Split_4k_Manual_EN.pdf
+ * 
+ * Inputs: RX, TX, Video, GND, VCC +(5-20V)
+ * 
+ * Outputs: Live Video
+ * 
+ * Set Up:
+ * 1. Connect Vcc to 5-20 V power source
+ * 2. Connect GND to GND of power source
+ * 3. Connect Video to a Video Transmitter
+ * 4. ... A lot more work to get something like this to work ...
+ * 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
