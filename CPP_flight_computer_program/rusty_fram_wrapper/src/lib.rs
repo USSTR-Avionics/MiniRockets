b93279b224c874c317bd81d56ddc65d4105893e0
home <adhyanpatel@gmail.com>
2022-10-24 01:54:54 -0600

    rust_temp func return a const arr and takes in c_float not tested yet

--- CPP_flight_computer_program/rusty_fram_wrapper/src/lib.rs
+++ CPP_flight_computer_program/rusty_fram_wrapper/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_main]
 #![no_std]
 
-use core::{convert::TryInto, ffi::{c_int, c_float}};
+use core::{ffi::{c_float}};
 // use core::panic::PanicInfo;
 // use core::time::Duration;
 use panic_halt as _;
@@ -28,36 +28,26 @@ pub extern "C" fn return_delay_test()  -> u8
     }
 
 #[no_mangle]
-pub extern "C" fn int_wrap_temperature_for_writing(temp_reading: c_float) -> u8 // take in a f32
+pub extern "C" fn int_wrap_temperature_for_writing(temp_reading: c_float) -> *const u8
     {
-    let temp_whole: i32 = temp_reading as i32;
-    
 
-    /* 
-    if (temp <= 100) && (temp >= 0)
+    let mut temp_whole: i32 = temp_reading as i32;
+    if (temp_whole >= -100) && (temp_whole <= 100)
         {
-        let unwrp_result: Result<u8, core::num::TryFromIntError> = temp.try_into(); 
-        let wrapped_temp: u8 = match unwrp_result
-            {
-            Ok(t) => t + 101,
-            _ => 0,
-            };
-        // wrapped_temp = wrapped_temp + 101;
-        return wrapped_temp;
+        temp_whole += 101;
         }
-    else if (temp <= 0) && (temp >= -100)
+    else
         {
-        let temp_conv = temp + 101;
-        let unwrp_result: Result<u8, core::num::TryFromIntError> = temp_conv.try_into(); 
-        let wrapped_temp: u8 = match unwrp_result
-            {
-            Ok(t) => t,
-            _ => 0,
-            };
-        // wrapped_temp = wrapped_temp + 101;
-        return wrapped_temp;
+        temp_whole = 0;
         }
-    */
 
-    return 0;
+    let mut temp_decimal: i32 = ((temp_reading * 100.0) as i32) % 100;
+    if (temp_decimal < 0) && (temp_decimal > 99)
+        {
+        temp_decimal = 0;
+        }
+
+    let temp_arr: [u8; 2] = [temp_whole as u8, temp_decimal as u8]; 
+
+    return temp_arr.as_ptr();
     }
