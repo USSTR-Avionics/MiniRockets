ed9dec98d631e9b5b813cce3f94f9737e9f9b546
Adhyan H <adhyanpatel@gmail.com>
2023-01-17 03:47:33 -0600

    0.9.2 - added compiler flags to platform.ini file

--- CPP_flight_computer_program/src/package_statemachine_t.cpp
+++ CPP_flight_computer_program/src/package_statemachine_t.cpp
@@ -5,24 +5,32 @@ statemachine_t::e_rocket_state set_state_for_statemachine(statemachine_t::e_rock
     switch(state) 
         {
         case 0:
+            rs = statemachine_t::e_rocket_state::unarmed;
             return statemachine_t::e_rocket_state::unarmed;
             break;
         case 1:
+            rs = statemachine_t::e_rocket_state::ground_idle;
             return statemachine_t::e_rocket_state::ground_idle;
             break;
         case 2:
+            rs = statemachine_t::e_rocket_state::powered_flight;
             return statemachine_t::e_rocket_state::powered_flight;
         case 3:
+            rs = statemachine_t::e_rocket_state::unpowered_flight;
             return statemachine_t::e_rocket_state::unpowered_flight;
         case 4:
+            rs = statemachine_t::e_rocket_state::ballistic_descent;
             return statemachine_t::e_rocket_state::ballistic_descent;
         case 5:
             return statemachine_t::e_rocket_state::chute_descent;
         case 6:
+            rs = statemachine_t::e_rocket_state::land_safe;
             return statemachine_t::e_rocket_state::land_safe;
         case 7:
+            rs = statemachine_t::e_rocket_state::test;
             return statemachine_t::e_rocket_state::test;
         default:
+            rs = statemachine_t::e_rocket_state::unarmed;
             return statemachine_t::e_rocket_state::unarmed;
         }
     }
