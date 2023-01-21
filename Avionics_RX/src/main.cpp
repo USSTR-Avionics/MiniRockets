1f2f6d47777dc024b026ec1094a6892293276f3b
kuang <tdkuang@gmail.com>
2023-01-21 14:49:49 -0600

    radio setup

--- Avionics_RX/src/main.cpp
+++ Avionics_RX/src/main.cpp
@@ -3,13 +3,26 @@
 #include <Wire.h>
 #include "RFM95W.h"
 
+
+RFM95W Radio(13, 32, 30);
+
+
 void setup()
 {
-    Serial.begin(9600); // arg doesnt need to be 9600 just true
+    Serial.begin(9600); // arg doesn't need to be 9600 just true
     Wire.begin();
+    Radio.Set_Frequency(914.1);
+
 }
 
 void loop()
 {
+    auto Message{Radio.Receive()};
+    if (std::get<0>(Message))
+    {
+        Serial.println("a");
+        Serial.println(std::get<1>(Message).c_str());
+    }
+
     // write your code here
 }
