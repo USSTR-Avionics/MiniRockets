3b13884b24def61e3895e1b6dfdc9d32373d10f5
Adhyan H <adhyanpatel@gmail.com>
2023-01-25 12:01:54 -0600

    fixed merge conflicts

--- CPP_flight_computer_program/src/package_fram.cpp
+++ CPP_flight_computer_program/src/package_fram.cpp
@@ -2,17 +2,13 @@
 #include <Arduino.h>
 #include <stdint.h>
 
-<<<<<<< HEAD
 #define ERR_READING  0
-=======
->>>>>>> master
 #define WRITE_OFFSET 0x10
 int fram_cursor = 0x10;
 
 int write_temperature_to_fram(float temp)
     {
     // TODO: test and work
-<<<<<<< HEAD
     float upper_bound = 100.0f;
     float lower_bound = -100.0f;
 
@@ -32,22 +28,11 @@ int write_temperature_to_fram(float temp)
     write_to_fram(fram_cursor, decimal_part);
     fram_cursor += 1;
 
-=======
-    int whole_part = (int)temp;
-    int decimal_part = (int)((temp - whole_part) * 1000);
-
-    write_to_fram(fram_cursor, whole_part);
-    fram_cursor += 1;
-    write_to_fram(fram_cursor, decimal_part);
-    fram_cursor += 1;
-
->>>>>>> master
     return EXIT_SUCCESS;
     }
 
 int write_acceleration_to_fram(float accel)
     {
-<<<<<<< HEAD
     // TODO: test and work
     uint8_t whole_part = (uint8_t)accel;
     uint8_t decimal_part = (uint8_t)((accel - whole_part) * 1000);
@@ -60,7 +45,3 @@ int write_acceleration_to_fram(float accel)
 
     return EXIT_SUCCESS;
     }
-=======
-    return EXIT_SUCCESS;
-    }
->>>>>>> master
