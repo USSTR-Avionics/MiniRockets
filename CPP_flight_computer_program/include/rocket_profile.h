a33a17306617ff3516d32703abce239dec262f7b
Adhyan H <adhyanpatel@gmail.com>
2023-01-30 18:25:48 -0600

    added more include guards

--- CPP_flight_computer_program/include/rocket_profile.h
+++ CPP_flight_computer_program/include/rocket_profile.h
@@ -1,3 +1,6 @@
+#ifndef ROCKET_PROFILE_H
+#define ROCKET_PROFILE_H
+
 // flight profile
 #define FLIGHT_MONTH            9
 #define FLIGHT_TIME_HOUR        11
@@ -5,3 +8,5 @@
 
 // rocket params
 #define ROCKET_HEIGHT 1 // in metres
+
+#endif // ROCKET_PROFILE_H
--- CPP_flight_computer_program/include/user_variables.h
+++ CPP_flight_computer_program/include/user_variables.h
@@ -1,6 +1,14 @@
-#define LIFTOFF_THRESHOLD                   11.0f // confirm with propulsion, nominal is 8.5 to 10.5
+#ifndef USER_VARIABLES_H
+#define USER_VARIABLES_H
+
+#if TESTPROFILE == 1
+    #define LIFTOFF_THRESHOLD               2.0f  // in ms^-2
+#endif // TESTPROFILE
+
+#define LIFTOFF_THRESHOLD                   11.0f  // in ms^-2
 #define DESCENT_CHECK_AMOUNT                2  
 #define ALTITUDE_CHANGE                     0.6f
-#define PARACHUTE_DEPLOYMENT_HEIGHT         4.0f
-#define LANDING_ALTITUDE                    1.0f
-#define STATE_TO_ENTER                      1    // ground idle
+#define PARACHUTE_DEPLOYMENT_HEIGHT         200.0f // in meters
+#define LANDING_ALTITUDE                    1.0f   // in meters
+
+#endif // USER_VARIABLES_H
