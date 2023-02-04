535dbc77d2d72615cbaad94e593304f8295509a5
Connor <lrk312@usask.ca>
2023-02-04 15:03:02 -0600

    Made write and read temperature work

--- CPP_flight_computer_program/src/package_fram.cpp
+++ CPP_flight_computer_program/src/package_fram.cpp
@@ -1,6 +1,7 @@
 #include "sensor_fram.h"
 #include <Arduino.h>
 #include <stdint.h>
+#include "debug_macros.h"
 
 #define ERR_READING 0
 int fram_cursor = 0x10;
@@ -13,23 +14,21 @@ void dump_fram_to_serial()
 int write_temperature_to_fram(float temp)
 	{
 	// TODO: test and work
-	float upper_bound = 100.0f;
-	float lower_bound = -100.0f;
+	float upper_bound = 127.0f;
+	float lower_bound = -128.0f;
 
 	if (temp > upper_bound || temp < lower_bound)
 		{
-		write_to_fram(fram_cursor, ERR_READING);
+		write_to_fram(ERR_READING, fram_cursor);
 		fram_cursor += 1;
 		return EXIT_FAILURE;
 		}
-
+	temp += 128;
 	uint8_t whole_part   = (uint8_t)temp;
-	uint8_t decimal_part = (uint8_t)((temp - whole_part) * 1000);
-	whole_part += 101;
-
-	write_to_fram(fram_cursor, whole_part);
+	uint8_t decimal_part = (uint8_t)((temp - whole_part) * 100);
+	write_to_fram(whole_part, fram_cursor);
 	fram_cursor += 1;
-	write_to_fram(fram_cursor, decimal_part);
+	write_to_fram(decimal_part, fram_cursor);
 	fram_cursor += 1;
 
 	return EXIT_SUCCESS;
@@ -38,11 +37,10 @@ int write_temperature_to_fram(float temp)
 float read_temperature_from_fram(int where)
 	{
 	// TODO:
-	int whole = read_from_fram(where);
+	uint8_t whole = read_from_fram(where);
 	where++; // read the next byte
-	int decimal       = read_from_fram(where);
-
-	float temperature = ((float)whole - 101.0f) + ((float)decimal / 100.0f);
+	uint8_t decimal       = read_from_fram(where);
+	float temperature = ((float)whole - 128.0f) + ((float)decimal / 100.0f);
 
 	return temperature;
 	}
@@ -56,13 +54,23 @@ int write_acceleration_to_fram(float accel)
 	{
 	// TODO: test and work
 	// handle negative accelation values
+	// TODO: test and work
+	float upper_bound = 127.0f;
+	float lower_bound = -128.0f;
+
+	if (accel > upper_bound || accel < lower_bound)
+		{
+		write_to_fram(ERR_READING, fram_cursor);
+		fram_cursor += 1;
+		return EXIT_FAILURE;
+		}
 	uint8_t whole_part   = (uint8_t)accel;
-	uint8_t decimal_part = (uint8_t)((accel - whole_part) * 1000);
+	uint8_t decimal_part = (uint8_t)((accel - whole_part) * 100);
 	whole_part += 101;
 
-	write_to_fram(fram_cursor, whole_part);
+	write_to_fram(whole_part, fram_cursor);
 	fram_cursor += 1;
-	write_to_fram(fram_cursor, decimal_part);
+	write_to_fram(decimal_part, fram_cursor);
 	fram_cursor += 1;
 
 	return EXIT_SUCCESS;
