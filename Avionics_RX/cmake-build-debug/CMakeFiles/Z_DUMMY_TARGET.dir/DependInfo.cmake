f90299f1d39eb89dba154fb9e51cb591fda70f4d
shadow <tdkuang@gmail.com>
2023-02-03 18:31:43 -0600

    gitignored cmake files and folders

---
+++ Avionics_RX/cmake-build-debug/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/RHutil/HardwareSerial.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/RHutil/HardwareSerial.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/RHutil/HardwareSerial.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/RHutil/RasPi.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/RHutil/RasPi.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/RHutil/RasPi.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/STM32ArduinoCompat/HardwareSPI.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/STM32ArduinoCompat/HardwareSPI.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/STM32ArduinoCompat/HardwareSPI.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/STM32ArduinoCompat/HardwareSerial.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/STM32ArduinoCompat/HardwareSerial.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/STM32ArduinoCompat/HardwareSerial.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/STM32ArduinoCompat/wirish.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/STM32ArduinoCompat/wirish.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/STM32ArduinoCompat/wirish.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/examples/raspi/RasPiRH.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/examples/raspi/RasPiRH.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/examples/raspi/RasPiRH.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHCRC.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHCRC.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHCRC.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHDatagram.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHDatagram.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHDatagram.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHGenericDriver.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHGenericDriver.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHGenericDriver.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHGenericSPI.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHGenericSPI.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHGenericSPI.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHHardwareSPI.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHHardwareSPI.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHHardwareSPI.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHMesh.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHMesh.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHMesh.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHNRFSPIDriver.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHNRFSPIDriver.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHNRFSPIDriver.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHReliableDatagram.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHReliableDatagram.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHReliableDatagram.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHRouter.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHRouter.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHRouter.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHSPIDriver.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHSPIDriver.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHSPIDriver.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RHSoftwareSPI.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHSoftwareSPI.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RHSoftwareSPI.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_ASK.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_ASK.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_ASK.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_CC110.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_CC110.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_CC110.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_MRF89.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_MRF89.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_MRF89.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_NRF24.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_NRF24.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_NRF24.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_NRF51.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_NRF51.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_NRF51.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_NRF905.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_NRF905.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_NRF905.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_RF22.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_RF22.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_RF22.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_RF24.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_RF24.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_RF24.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_RF69.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_RF69.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_RF69.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_RF95.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_RF95.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_RF95.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_Serial.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_Serial.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_Serial.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/src/RH_TCP.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_TCP.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/src/RH_TCP.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/lib/RadioHead/tools/simMain.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/tools/simMain.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/lib/RadioHead/tools/simMain.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/src/RFM95W.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/src/RFM95W.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/src/RFM95W.cpp.obj.d"
+  "D:/ktz/Programming/Club/USST/Avionics/Avionics-code/Avionics_RX/src/main.cpp" "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
