02bf8a7461d50d03d6fb21689f7644b63105b434
Adhyan H <adhyanpatel@gmail.com>
2023-02-12 10:02:01 -0600

    test

---
+++ CPP_flight_computer_program/commit.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+current_branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'`
+commit_message=""
+
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: commit.sh <commit message>"
+    exit 1
+fi
+
+if [ $current_branch == "(dev)" ]
+then
+    echo "You are on dev branch"
+    `git status`
+    `git add .`
+    `git commit -m "$1"`
+    `git push origin dev`
+else
+    echo "You are NOT on (dev) branch"
+fi
