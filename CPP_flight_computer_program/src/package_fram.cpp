2222970d7707ec9393423b56f422586cc75fbdf5
Adhyan H <adhyanpatel@gmail.com>
2023-02-12 01:32:44 -0600

    fixed compilation error in package_fram

--- CPP_flight_computer_program/src/package_fram.cpp
+++ CPP_flight_computer_program/src/package_fram.cpp
@@ -1,14 +1,14 @@
-#include "debug_macros.h"
 #include "sensor_fram.h"
 #include <Arduino.h>
-#include <cstdint>
 #include <stdint.h>
+#include "debug_macros.h"
+#include <cstdint>
 
 #define ERR_READING 0
 int fram_cursor         = 0x10;
 
-const float upper_bound = 100.0F;
-const float lower_bound = -100.0F;
+// const float upper_bound = 100.0F;
+// const float lower_bound = -100.0F;
 
 
 void dump_fram_to_serial()
@@ -33,14 +33,8 @@ auto write_temperature_to_fram(float temp) -> int
 	temp += 128;
 	uint8_t whole_part   = (uint8_t)temp;
 	uint8_t decimal_part = (uint8_t)((temp - whole_part) * 100);
+
 	write_to_fram(whole_part, fram_cursor);
-
-
-	auto whole_part   = (uint8_t)temp;
-	auto decimal_part = (uint8_t)((temp - whole_part) * 1000);
-	whole_part += 101;
-
-	write_to_fram(fram_cursor, whole_part);
 	fram_cursor += 1;
 	write_to_fram(decimal_part, fram_cursor);
 	fram_cursor += 1;
@@ -53,7 +47,7 @@ auto read_temperature_from_fram(int where) -> float
 	// TODO:
 	uint8_t whole = read_from_fram(where);
 	where++; // read the next byte
-	uint8_t decimal   = read_from_fram(where);
+	uint8_t decimal       = read_from_fram(where);
 	float temperature = ((float)whole - 128.0f) + ((float)decimal / 100.0f);
 
 	return temperature;
@@ -68,7 +62,7 @@ auto write_acceleration_to_fram(float accel) -> int
 	{
 	// TODO: test and work
 	// handle negative accelation values
-
+  
 	float upper_bound = 127.0f;
 	float lower_bound = -128.0f;
 
@@ -97,3 +91,4 @@ auto read_acceleration_from_fram(int where) -> float
 	where = 0;
 	return 0.0F;
 	}
+
