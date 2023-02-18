2f902072e2148ad49e6f8a86f46de8415d45569e
Adhyan H <adhyanpatel@gmail.com>
2023-02-18 00:57:25 -0600

    sync that builds

--- CPP_flight_computer_program/src/main.cpp
+++ CPP_flight_computer_program/src/main.cpp
@@ -481,11 +481,6 @@ void setup()
 	// rocket_state = set_state_for_statemachine(&rocket_state, value_from_fram);
 
 	println("setup() exit");
-
-    if (start_counter_time  == 0UL)
-        {
-        start_counter_time = millis();
-        }
 	}
 
 void loop()
