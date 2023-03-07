3f7fbc02d97107974e8adf27073c62f38666c87b
Adhyan H <adhyanpatel@gmail.com>
2023-03-07 15:30:45 -0600

    0.10.5 - manual merge of [PR #60]

--- CPP_flight_computer_program/include/sensor_bmp280.h
+++ CPP_flight_computer_program/include/sensor_bmp280.h
@@ -1,13 +1,8 @@
 #ifndef SENSOR_BMP280_H
 #define SENSOR_BMP280_H
 
-<<<<<<< HEAD
 #define APOGEE_BUFFER_SIZE          5
 #define APOGEE_READING_INTERVAL     50
-=======
-#define APOGEE_BUFFER_SIZE          50
-#define APOGEE_READING_INTERVAL     75
->>>>>>> 92e250922d1cecc831c49455a9cba505e00a16b4
 #define APOGEE_DIFFERENCE_THRESHOLD 0.10f
 
 extern float ground_base_pressure;
