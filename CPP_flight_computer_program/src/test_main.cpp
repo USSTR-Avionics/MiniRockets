90ce0a5829d675658f8677a94914547671359f1d
Adhyan H <adhyanpatel@gmail.com>
2023-03-12 12:35:53 -0600

    sync

--- CPP_flight_computer_program/src/test_main.cpp
+++ CPP_flight_computer_program/src/test_main.cpp
@@ -18,35 +18,44 @@ void teardown_test_sensor_fram()
 
 void test_sensor_fram()
 	{
-	write_data_chunk_to_fram(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-	read_data_chunk_from_fram(FRAM_INIT_ADDRESS);
+    int test_cursor = FRAM_INIT_ADDRESS;
+    int count = 0;
 
-	println(sensor_chunk.timestamp);
-	println(sensor_chunk.current_state);
-	println(sensor_chunk.accl_x);
-	println(sensor_chunk.accl_y);
-	println(sensor_chunk.accl_z);
-	println(sensor_chunk.gyro_x);
-	println(sensor_chunk.gyro_y);
-	println(sensor_chunk.gyro_z);
-	println(sensor_chunk.rel_alt);
-	println(sensor_chunk.pressure);
-	println(sensor_chunk.thermocouple_temp);
+    while (count < 10)
+        {
+	    write_data_chunk_to_fram(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	    read_data_chunk_from_fram(test_cursor);
 
-	write_data_chunk_to_fram(millis(), 6, 0.12f, 0.24f, 9.8f, 0.0f, 45.5f, 90.0f, 123.456f, 45000.75, 223.56f);
-	read_data_chunk_from_fram(FRAM_INIT_ADDRESS + 25);
+	    println(sensor_chunk.timestamp);
+	    println(sensor_chunk.current_state);
+	    println(sensor_chunk.accl_x);
+	    println(sensor_chunk.accl_y);
+	    println(sensor_chunk.accl_z);
+	    println(sensor_chunk.gyro_x);
+	    println(sensor_chunk.gyro_y);
+	    println(sensor_chunk.gyro_z);
+	    println(sensor_chunk.rel_alt);
+	    println(sensor_chunk.pressure);
+	    println(sensor_chunk.thermocouple_temp);
 
-	println(sensor_chunk.timestamp);
-	println(sensor_chunk.current_state);
-	println(sensor_chunk.accl_x);
-	println(sensor_chunk.accl_y);
-	println(sensor_chunk.accl_z);
-	println(sensor_chunk.gyro_x);
-	println(sensor_chunk.gyro_y);
-	println(sensor_chunk.gyro_z);
-	println(sensor_chunk.rel_alt);
-	println(sensor_chunk.pressure);
-	println(sensor_chunk.thermocouple_temp);
+	    write_data_chunk_to_fram(millis(), 6, 0.12f, 0.24f, 9.8f, 0.0f, 45.5f, 90.0f, 123.456f, 45000.75, 223.56f);
+	    read_data_chunk_from_fram(test_cursor+ 25);
+
+	    println(sensor_chunk.timestamp);
+	    println(sensor_chunk.current_state);
+	    println(sensor_chunk.accl_x);
+	    println(sensor_chunk.accl_y);
+	    println(sensor_chunk.accl_z);
+	    println(sensor_chunk.gyro_x);
+	    println(sensor_chunk.gyro_y);
+	    println(sensor_chunk.gyro_z);
+	    println(sensor_chunk.rel_alt);
+	    println(sensor_chunk.pressure);
+	    println(sensor_chunk.thermocouple_temp);
+
+        count++;
+        count++;
+        }
 	}
 
 int test_main()
