2ac5236bb3e18b0d4351606fa83f624b82af1521
JackEhttack <69522615+JackEhttack@users.noreply.github.com>
2023-03-18 14:04:49 -0600

    fram problem fixed! Switched 8bit -> 16bit (#67)
    Co-authored-by: Jack Donegan <lvf165@usask.ca>
    Co-authored-by: Adhyan H. Patel <67227078+frroossst@users.noreply.github.com>

--- CPP_flight_computer_program/src/test_main.cpp
+++ CPP_flight_computer_program/src/test_main.cpp
@@ -18,9 +18,52 @@ void teardown_test_sensor_fram()
 
 void test_sensor_fram()
 	{
+
+    write_data_chunk_to_fram(1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1);
+    read_data_chunk_from_fram(FRAM_INIT_ADDRESS);
+
+    println(sensor_chunk.timestamp);
+    println(sensor_chunk.current_state);
+    println(sensor_chunk.accl_x);
+    println(sensor_chunk.accl_y);
+    println(sensor_chunk.accl_z);
+    println(sensor_chunk.gyro_x);
+    println(sensor_chunk.gyro_y);
+    println(sensor_chunk.gyro_z);
+    println(sensor_chunk.rel_alt);
+    println(sensor_chunk.pressure);
+    println(sensor_chunk.thermocouple_temp);
+
+    for (int i = 1; i <= 100; i++)
+    {
+        
+        write_data_chunk_to_fram(millis(), 6 + i, 0.12f + i / 10, 0.24f, 9.8f, 0.0f, 45.5f, 90.0f, 123.456f, 45000.75, 223.56f);
+
+    }
+
+    for (int a = 1; a <= 100; a++)
+    {
+        read_data_chunk_from_fram(FRAM_INIT_ADDRESS + 25 * a);
+
+        println(sensor_chunk.timestamp);
+        println(sensor_chunk.current_state);
+        println(sensor_chunk.accl_x);
+        println(sensor_chunk.accl_y);
+        println(sensor_chunk.accl_z);
+        println(sensor_chunk.gyro_x);
+        println(sensor_chunk.gyro_y);
+        println(sensor_chunk.gyro_z);
+        println(sensor_chunk.rel_alt);
+        println(sensor_chunk.pressure);
+        println(sensor_chunk.thermocouple_temp);
+    }
+
+    println("finished");
+
 	int test_cursor = FRAM_INIT_ADDRESS + 100;
 	fram_cursor     = FRAM_INIT_ADDRESS + 100;
 	int count       = 0;
+
 
 	for (int i = FRAM_INIT_ADDRESS; i < FRAM_INIT_ADDRESS + 50; i += 25)
 		{
