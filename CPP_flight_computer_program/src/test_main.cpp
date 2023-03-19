69c622738e1ea8077d85b0b756f091647810f06a
Adhyan H <adhyanpatel@gmail.com>
2023-03-18 22:06:26 -0600

    end position is finnick

--- CPP_flight_computer_program/src/test_main.cpp
+++ CPP_flight_computer_program/src/test_main.cpp
@@ -18,92 +18,32 @@ void teardown_test_sensor_fram()
 
 void test_sensor_fram()
 	{
+    int upper = 2500;
+    // write to FRAM
+    for (int i = 0; i < upper; i++)
+        {
+        write_data_chunk_to_fram(millis(), 2, 0.5f, 0.5f, 9.8f, 0.0f, 90.0f, 180.0f, 100, 45000.0f, 35.5f);
+        }
 
-	write_data_chunk_to_fram(1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1);
-	read_data_chunk_from_fram(FRAM_INIT_ADDRESS);
-
-	println(sensor_chunk.timestamp);
-	println(sensor_chunk.current_state);
-	println(sensor_chunk.accl_x);
-	println(sensor_chunk.accl_y);
-	println(sensor_chunk.accl_z);
-	println(sensor_chunk.gyro_x);
-	println(sensor_chunk.gyro_y);
-	println(sensor_chunk.gyro_z);
-	println(sensor_chunk.rel_alt);
-	println(sensor_chunk.pressure);
-	println(sensor_chunk.thermocouple_temp);
-
-	for (int i = 1; i <= 100; i++)
-		{
-
-		write_data_chunk_to_fram(millis(), 6 + i, 0.12f + i / 10, 0.24f, 9.8f, 0.0f, 45.5f, 90.0f, 123.456f, 45000.75, 223.56f);
-		}
-
-	for (int a = 1; a <= 100; a++)
-		{
-		read_data_chunk_from_fram(FRAM_INIT_ADDRESS + 25 * a);
-
-		println(sensor_chunk.timestamp);
-		println(sensor_chunk.current_state);
-		println(sensor_chunk.accl_x);
-		println(sensor_chunk.accl_y);
-		println(sensor_chunk.accl_z);
-		println(sensor_chunk.gyro_x);
-		println(sensor_chunk.gyro_y);
-		println(sensor_chunk.gyro_z);
-		println(sensor_chunk.rel_alt);
-		println(sensor_chunk.pressure);
-		println(sensor_chunk.thermocouple_temp);
-		}
-
-	println("finished");
-
-	int test_cursor = FRAM_INIT_ADDRESS + 100;
-	fram_cursor     = FRAM_INIT_ADDRESS + 100;
-	int count       = 0;
-
-
-	for (int i = FRAM_INIT_ADDRESS; i < FRAM_INIT_ADDRESS + 50; i += 25)
-		{
-		println("writing data chunk to fram at position: " + String(i));
-		write_data_chunk_to_fram(millis(), 6, 0.12f, 0.24f, 9.8f, 0.0f, 45.5f, 90.0f, 123.456f, 45000.75, 223.56f);
-
-		read_data_chunk_from_fram(i - 25);
-		println("reading data chunk from fram at position: " + String(i));
-
-		// print sensor chunk
-		println(sensor_chunk.timestamp);
-		println(sensor_chunk.current_state);
-		println(sensor_chunk.accl_x);
-		println(sensor_chunk.accl_y);
-		println(sensor_chunk.accl_z);
-		println(sensor_chunk.gyro_x);
-		println(sensor_chunk.gyro_y);
-		println(sensor_chunk.gyro_z);
-		println(sensor_chunk.rel_alt);
-		println(sensor_chunk.pressure);
-		println(sensor_chunk.thermocouple_temp);
-		}
-
-	for (int i = FRAM_INIT_ADDRESS; i < FRAM_INIT_ADDRESS + 50; i += 25)
-		{
-		read_data_chunk_from_fram(i);
-		println("reading data chunk from fram at position: " + String(i));
-
-		// print sensor chunk
-		println(sensor_chunk.timestamp);
-		println(sensor_chunk.current_state);
-		println(sensor_chunk.accl_x);
-		println(sensor_chunk.accl_y);
-		println(sensor_chunk.accl_z);
-		println(sensor_chunk.gyro_x);
-		println(sensor_chunk.gyro_y);
-		println(sensor_chunk.gyro_z);
-		println(sensor_chunk.rel_alt);
-		println(sensor_chunk.pressure);
-		println(sensor_chunk.thermocouple_temp);
-		}
+    // read from FRAM
+    uint16_t test_cursor = FRAM_INIT_ADDRESS;
+    for (int i = 0; i < upper; i++)
+        {
+        read_data_chunk_from_fram(test_cursor);
+        test_cursor += 25;
+        println("test_cursor: " +String(test_cursor));
+        println("millis: " + String(sensor_chunk.timestamp));
+        println("current state: " + String(sensor_chunk.current_state));
+        println("accl x: " + String(sensor_chunk.accl_x));
+        println("accl y: " + String(sensor_chunk.accl_y));
+        println("accl z: " + String(sensor_chunk.accl_z));
+        println("gyro x: " + String(sensor_chunk.gyro_x));
+        println("gyro y: " + String(sensor_chunk.gyro_y));
+        println("gyro z: " + String(sensor_chunk.gyro_z));
+        println("rel alt: " + String(sensor_chunk.rel_alt));
+        println("pressure: " + String(sensor_chunk.pressure));
+        println("temp: " + String(sensor_chunk.thermocouple_temp));
+        }
 	}
 
 int test_main()
