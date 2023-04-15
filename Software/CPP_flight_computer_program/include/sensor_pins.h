d22e61d43c4b1eeaeac6fad5dafbbbb3c1be160c
Adhyan H <adhyanpatel@gmail.com>
2023-04-15 13:04:16 -0600

    1.1.0 - pin layout changes, yet to be tested

--- Software/CPP_flight_computer_program/include/sensor_pins.h
+++ Software/CPP_flight_computer_program/include/sensor_pins.h
@@ -4,12 +4,12 @@
 // define all pins here so that it is easier to change them
 
 // LED pins
-#define LED_RED       37
 #define LED_BLUE      38
 #define LED_GREEN     39
+#define LED_RED       40
 
 // Buzzer pins
-#define BUZZER_PIN    8
+#define BUZZER_PIN    14
 
 // Thermocouple pins
 #define MAXDO         12
@@ -17,7 +17,7 @@
 #define MAXCLK        27
 
 // Parachute pins
-#define PARACHUTE_PIN 2
+#define PARACHUTE_PIN 25
 
 
 #endif // SENSOR_PINS_H_
